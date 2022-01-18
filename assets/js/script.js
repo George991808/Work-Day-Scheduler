785c434505d75f09ef5bb37d74e16baa0d59a09c
George Ellis <foc18es@alcoa.com>
2022-01-18 11:24:57 +0800

    added event listener to buttons, used moment to get date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,32 @@
-
 var planner = document.getElementById("planner");
-buildplanner()
+// var savedInputs= localStorage.getItem("savedNote")) || [];
+buildplanner();
 function buildplanner() {
-  
-  
-    for (let i = 0; i < 24; i++) {
-      var row = document.createElement("div");
-      row.setAttribute("class", "row");
-     
-      planner.appendChild(row);
-      var timeslot = document.createElement("div");
-      timeslot.innerText=i+":00";
-       row.appendChild(timeslot);
+  for (let i = 0; i < 24; i++) {
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
 
-       var input = document.createElement("input");
-       input.setAttribute("type", "text");
-       row.appendChild(input);
+    planner.appendChild(row);
+    var timeslot = document.createElement("div");
+    timeslot.innerText = i + ":00";
+    row.appendChild(timeslot);
 
-       var save = document.createElement("button");
-      save.innerText="SAVE"
-       row.appendChild(save);
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    row.appendChild(input);
 
-    }
-  
-    
+    var save = document.createElement("button");
+    save.innerText = "SAVE";
+    row.appendChild(save);
+    save.addEventListener("click", saveInput);
   }
+}
+
+function saveInput(event) {
+  alert("Hello! I am an alert box!!");
+}
+
+//show current date in header
+document.getElementsByClassName("date")[0].innerText =
+  moment().format("MMMM Do YYYY");
+//
